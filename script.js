020a26cf67b95a449697bd30a9733019a93460e4
Gabriel Baumann <gabrielgoncalves238@gmail.com>
2025-04-26 18:17:09 -0300

    Feat: add a news card

--- script.js
+++ script.js
@@ -55,4 +55,13 @@ document.addEventListener('DOMContentLoaded', function() {
     
     window.addEventListener('scroll', highlightNavItem);
     highlightNavItem();
+
+    // efeito news 
+    document.querySelector('.group').addEventListener('mousemove', function(e) {
+        const rect = this.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        this.style.setProperty('--mouse-x', `${x}px`);
+        this.style.setProperty('--mouse-y', `${y}px`);
+    });
 });
